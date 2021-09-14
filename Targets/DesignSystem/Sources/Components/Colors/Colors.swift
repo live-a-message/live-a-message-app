0d44c90d1ac8a3be28ebb76dbc98bb982e7f6216
Fernando de Lucas <fernando.gomes@hp.com>
2021-09-14 11:45:02 -0300

    chore: add hex support for UIColors

--- Targets/DesignSystem/Sources/Components/Colors/Colors.swift
+++ Targets/DesignSystem/Sources/Components/Colors/Colors.swift
@@ -16,7 +16,43 @@ public class Colors: UIColor {
   /// the default red color
   public static var mainRed: UIColor {
     return UIColor { (traitCollection) -> UIColor in
-              return traitCollection.userInterfaceStyle == .dark ? UIColor(red: CGFloat(237) / 255.0, green: CGFloat(110) / 255.0, blue: CGFloat(126) / 255.0, alpha: 1.0) : UIColor(red: CGFloat(248) / 255.0, green: CGFloat(95) / 255.0, blue: CGFloat(113) / 255.0, alpha: 1.0)
+              return traitCollection.userInterfaceStyle == .dark ? UIColor(hex: "ED6E7E") : UIColor(hex: "F85F71")
       }
   }
+  
+  /// the secondary  red color used for pop-up label over label in mainred components
+  public static var shadowRed: UIColor {
+    return UIColor(hex: "FDD2D7")
+  }
+}
+
+extension UIColor {
+    
+    convenience init(hex: String, alpha: CGFloat = 1.0) {
+        var hexFormatted: String = hex.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines).uppercased()
+
+        if hexFormatted.hasPrefix("#") {
+            hexFormatted = String(hexFormatted.dropFirst())
+        }
+
+        assert(hexFormatted.count == 6, "Invalid hex code used.")
+
+        var rgbValue: UInt64 = 0
+        Scanner(string: hexFormatted).scanHexInt64(&rgbValue)
+
+        self.init(red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+                  green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+                  blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+                  alpha: alpha)
+    }
+    
+    func toHexString() -> String {
+        var r:CGFloat = 0
+        var g:CGFloat = 0
+        var b:CGFloat = 0
+        var a:CGFloat = 0
+        getRed(&r, green: &g, blue: &b, alpha: &a)
+        let rgb:Int = (Int)(r*255)<<16 | (Int)(g*255)<<8 | (Int)(b*255)<<0
+        return String(format:"#%06x", rgb)
+    }
 }
