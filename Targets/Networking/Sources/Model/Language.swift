c4cd67342418cf19610d6e60cbeccc342a8abe48
Tales Conrado <talesmateuscc@gmail.com>
2021-10-06 08:41:26 -0300

    Update Language.swift

--- Targets/Networking/Sources/Model/Language.swift
+++ Targets/Networking/Sources/Model/Language.swift
@@ -14,7 +14,7 @@ enum Language: String {
     case en
     case znh
 
-    /// Get a locale language if the langugage is not avaiable, the default would me english.
+    /// Get a locale language if the langugage is not avaiable, the default would be english.
     /// - Returns: One available languange based on preferredLanguage
     static func locale() -> Language {
         let preferredLanguage = Locale.preferredLanguages.first as String?
