5f3a3bfeadbfdb9dbcdc9222a92df0b97bdac636
Vinicius Mesquita <viniciuscoelho.ti@gmail.com>
2021-11-03 09:36:22 -0300

    feat: change profile

--- Targets/LiveAMessage/Sources/Scenes/Profile/ViewModel/ProfileViewModel.swift
+++ Targets/LiveAMessage/Sources/Scenes/Profile/ViewModel/ProfileViewModel.swift
@@ -14,30 +14,18 @@ protocol ProfileViewModelProtocol {
 
 struct ProfileViewModel: ProfileViewModelProtocol {
 
+    let items: [ProfileModel] = [
+        ProfileModel(type: .termsOfService)
+    ]
+
     var sections: [[ProfileCellViewModel]] = []
 
     init() {
-        let items: [ProfileModel] = makeItems {
-            ProfileModel(type: .termsOfService)
-        }
+
         let firstSection = items.map({ ProfileCellViewModel(model: $0) })
         sections.append(firstSection)
         sections.append(contentsOf: Array(repeating: [], count: 8))
         let lastSection = [ProfileCellViewModel(model: ProfileModel(type: .logout))]
         sections.append(lastSection)
-    }
-
-    func makeItems(@ItemsBuilder _ content: () -> [ProfileModel]) -> [ProfileModel] {
-        content()
-    }
-}
-
-@resultBuilder
-struct ItemsBuilder {
-    static func buildBlock() -> [ProfileModel] { [] }
-}
-extension ItemsBuilder {
-    static func buildBlock(_ items: ProfileModel...) -> [ProfileModel] {
-        items
     }
 }
