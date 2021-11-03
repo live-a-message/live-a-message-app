809694e0dd00f2610bad78ded84e7f1337d3479b
Albert Rayneer <rayneerqueiroz@hotmail.com>
2021-11-03 11:33:37 -0300

    fix (Message Pin): fix message pin image when dequeu anottation

--- Targets/LiveAMessage/Sources/Scenes/Map/View/MapView.swift
+++ Targets/LiveAMessage/Sources/Scenes/Map/View/MapView.swift
@@ -60,12 +60,12 @@ extension MapView: MKMapViewDelegate {
         var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier)
         if annotationView == nil {
             annotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
-            let image = AkeeAsset.marker.image
-            annotationView?.image = image
             annotationView?.canShowCallout = true
         } else {
             annotationView?.annotation = annotation
         }
+        let image = AkeeAsset.marker.image
+        annotationView?.image = image
         return annotationView
     }
 
